ff7f022a1d2f0b7ce244f521902fd9cff9b5412e
umutanyeri <67645453+umutanyeri@users.noreply.github.com>
2022-10-22 13:43:55 +0300

    corrected analytical solution for inf.pot.well
    derived properly the projection constant for every eigenvector

--- scripts/inf-well.py
+++ scripts/inf-well.py
@@ -7,17 +7,24 @@ from scipy.special import erf
 
 def analytic(x,t):
     psi = x*0j
+#add eigenvectors as many as number of spatial point
     for n in range(len(x)):
-        kn = -n*np.pi/(2*xw)
+#calculate allowed momentum and corresponding energies
+        kn = n*np.pi/(2*xw)
         en = kn**2/2
-        psi_k = 1/(2*np.pi) * np.exp(-s**2 * (kn-k0)**2 - 1j*(kn-k0)*x0) * np.sqrt(np.pi)/2/s
-        #* (erf( (xw - x0 + 2j*s**2*(kn-k0))/2/s ) - erf( (-xw - x0 + 2j*s**2*(kn-k0))/2/s ))
-        psi += psi_k * np.exp(1j*x*kn - 1j*en*t)
-        if not n == 0:
-            psi_k = 1/(2*np.pi) * np.exp(-s**2 * (-kn-k0)**2 - 1j*(-kn-k0)*x0) * np.sqrt(np.pi)/2/s
-            #* (erf( (xw - x0 + 2j*s**2*(-kn-k0))/2/s ) - erf( (-xw - x0 + 2j*s**2*(-kn-k0))/2/s ))
-            psi += psi_k * np.exp(-1j*x*kn - 1j*en*t)
-    return psi
+#calculate projections to gaussian with initial position x_0 and momentum k_0
+#projection to e^{ikx}
+        A_n = np.exp(-s**2 * (kn-k0)**2 - 1j*(kn-k0)*x0) * np.sqrt(np.pi)*2*s
+#projection to e^{-ikx}
+        B_n = np.exp(-s**2 * (-kn-k0)**2 - 1j*(-kn-k0)*x0) * np.sqrt(np.pi)*2*s
+#if the integer n is even calculate the constant in front of the sine
+        if(n % 2 == 0):
+            psi += 1j*(A_n-B_n) * np.sin(x*kn) *np.exp(-1j*en*t)
+#if the integer n is odd calculate the constant in front of the cosine
+        else:
+            psi += (A_n+B_n) * np.cos(x*kn) *np.exp(-1j*en*t)
+#scale the solution with width of the well (normalization factor of FT)
+    return psi / (2*xw)
 
 #define position grid and potential term
 xw = 20 #half of the width of well centered at origin
@@ -44,24 +51,26 @@ N_fig_y = 2
 fig,ax = plt.subplots(N_fig_y , N_fig_x , figsize=((N_fig_x+1)*5/2.54,(N_fig_y+1)*5/2.54) , tight_layout=True)
 
 #evolve w.f.
-Dt=.4  #time difference between subfigures
+Dt=1.2  #time difference between subfigures
 dt = dx**2/4 #time difference between steps in time evolution
 for sf in range(N_fig_x*N_fig_y):
 #locate the target subfigure
     i = sf // N_fig_x
     j = sf % N_fig_x
-#plot inf. potential well
+#calculate the actual time in the plot
     t = (sf+1)*Dt
-    ax[i][j].plot(x,v , label="v(x)")
+#plot the analytical solution
+    ax[i][j].plot(x, analytic(x,t).real , lw=2,label="analytic")
 #evolve the w.f.
+#w/split-step fourier method
     psi1 = SSF(psi1 , x , Dt , v , dt)
-#    ax[i][j].plot(x,abs(psi1) , label="SSF")
-    ax[i][j].plot(x,psi1.real , label="SSF")
+#w/crank-nicolson method
     psi2 = CN(psi2 , x , Dt , v , dt)
-#    ax[i][j].plot(x,abs(psi2) , label="CN")
-    ax[i][j].plot(x,psi2.real , label="CN")
-#    ax[i][j].plot(x,abs( analytic(x,t) ) , label="analytic")
-    ax[i][j].plot(x, analytic(x,t).real , label="analytic")
+#plot numerical solutions
+    ax[i][j].plot(x,psi1.real , lw=1, label="SSF")
+    ax[i][j].plot(x,psi2.real , lw=1, label="CN")
+#plot inf. potential well
+    ax[i][j].plot(x,v , label="v(x)")
 
     ax[i][j].set_title("t=%.3f"%(t))
     ax[i][j].grid(alpha=.3)
